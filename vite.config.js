d5d8f5e53217709dbee6fc5b72e5f9787630a969
JoaoPMV <joaop.trilha@gmail.com>
2026-08-20 00:24:00 -0300

    Remove arquivos obsoletos

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      manifest: {
-        name: "Crossword Game",
-        short_name: "Crossword",
-        description: "Educational crossword game to learn English",
-        theme_color: "#000000",
-        background_color: "#000000",
-        display: "standalone",
-        orientation: "landscape",
-        start_url: "/",
-        icons: [
-          {
-            src: "/pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
   server: {
     port: 3000,
   },
